e21e858f124bc0fc621fa67a11d12bb2f26d51fa
Bradley-A-Davis <bradleydavis0411@gmail.com>
2025-02-15 23:44:07 -0600

    g

--- hi.py
+++ hi.py
@@ -1,8 +1,8 @@
 import torch
-import xformers
-from xformers.ops import memory_efficient_attention
+print("Torch Version:", torch.__version__)
+print("CUDA Available:", torch.cuda.is_available())
+print("CUDA Version:", torch.version.cuda)
 
-print("CUDA Available:", torch.cuda.is_available())  # Should return True
-print("CUDA Version:", torch.version.cuda)  # Should be 12.1
-print("PyTorch Version:", torch.__version__)  # Should be 2.1.0+cu121
-print("xFormers Version:", xformers.__version__)  # Should print a version
+import xformers
+print("xFormers Version:", xformers.__version__)
+
